e6c442716aa0d45d2847c3e223b434488e3092b5
Vradhi Bishnoi <vradhiabishnoi@gmail.com>
2025-03-25 13:16:53 +0530

    modifying main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ module "dev-infra"{
     source = "./infra-app"
     env = "dev"
     bucket_name = "vradhi-infra-app-bucket"
-    ec2_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOmHl2kkHGCH99n47aNPyanRHO7F8tLlpOr5FCEaAqJG vradhi bishnoi@DESKTOP-DJDJFNU"
+    ec2_key = file("ec2-key-pair.pub")
     instance_count = 1
     instance_type = "t2.micro"
     instance_ami_id = "ami-084568db4383264d4"
@@ -14,7 +14,7 @@ module "prd-infra"{
     source = "./infra-app"
     env = "prd"
     bucket_name = "vradhi-infra-app-bucket"
-    ec2_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOmHl2kkHGCH99n47aNPyanRHO7F8tLlpOr5FCEaAqJG vradhi bishnoi@DESKTOP-DJDJFNU"
+    ec2_key = file("ec2-key-pair.pub")
     instance_count = 2
     instance_type = "t2.medium"
     instance_ami_id = "ami-084568db4383264d4"
@@ -26,7 +26,7 @@ module "stg-infra"{
     source = "./infra-app"
     env = "stg"
     bucket_name = "vradhi-infra-app-bucket"
-    ec2_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOmHl2kkHGCH99n47aNPyanRHO7F8tLlpOr5FCEaAqJG vradhi bishnoi@DESKTOP-DJDJFNU"
+    ec2_key = file("ec2-key-pair.pub")
     instance_count = 1
     instance_type = "t2.small"
     instance_ami_id = "ami-084568db4383264d4"
